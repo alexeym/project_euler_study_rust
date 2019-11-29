bd6d6f8bb15995863f23950a4e165bc8c38db9a3
Alexey M <alexeym@users.noreply.github.com>
2019-11-28 20:43:52 -0800

    Solve problem 6

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,24 @@ fn main() {
 
     let p5 = p5_smallest_multiple();
     assert!(p5 == 232792560, "P5 expected {}, result {}", 232792560, p5);
+
+    let p6 = p6_sum_square_difference();
+    assert!(p6 == 25164150, "P6 expected {}, result {}", 25164150, p6);
+
+}
+
+fn p6_sum_square_difference() -> i32 {
+    let max: i32 = 100;
+    let mut sum = 0;
+    let mut sum_squares = 0;
+    for num in 1..=max {
+        sum += num;
+        sum_squares += num.pow(2);
+    }
+    return sum.pow(2) - sum_squares;
 }
 
 fn p5_smallest_multiple() -> i64 {
-
     let mut num = 0;
     let nums = [6, 9, 11, 12, 13, 14, 15, 16];
     loop {
